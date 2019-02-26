3bde5a002a0b7f32b3efe979be5abe17dd6629fb
Sebastien Langoureaux <sebastien.langoureaux@harmonie-mutuelle.fr>
2019-02-26 16:48:29 +0000

    Fix provider version

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ data "aws_ami" "windows_server" {
 
 # Create repository instance
 module "instance_ec2" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance"
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance?ref=v1.15.0"
 
   name                        = "${var.instance_name}"
   instance_count              = 1
